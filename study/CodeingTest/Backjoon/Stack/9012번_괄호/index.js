ae9ad1918341a6d067483030a206baa0da19a230
tjrehdrms123 <wwe5455@naver.com>
2023-04-08 19:15:13 +0900

    Update

--- study/CodeingTest/Backjoon/Stack/9012번_괄호/index.js
+++ study/CodeingTest/Backjoon/Stack/9012번_괄호/index.js
@@ -1,21 +1,21 @@
-let fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().split('\n');
+let fs = require("fs");
+let input = fs.readFileSync("./input.txt").toString().split("\n");
 
 const inputLength = Number(input[0]);
 
-let result =  []; // 결과 배열
+let result = []; // 결과 배열
 
 for (let i = 1; i <= inputLength; i++) {
   let leftBracket = []; // 왼쪽 괄호
   let rightBracket = []; // 오른쪽 괄호
   let temp = []; // 쌍을 없애고 남은 괄호 모음
   for (let j = 0; j < input[i].length; j++) {
-    if (input[i][j] == '('){
+    if (input[i][j] == "(") {
       leftBracket.push(input[i][j]);
       temp.push(input[i][j]);
     }
-    if(input[i][j] == ')'){
-      if(temp.length != 0 && temp[0] != ')'){
+    if (input[i][j] == ")") {
+      if (temp.length != 0 && temp[0] != ")") {
         rightBracket.push(input[i][j]);
         temp.pop();
       } else {
@@ -25,10 +25,10 @@ for (let i = 1; i <= inputLength; i++) {
       }
     }
   }
-  if(leftBracket.length === 0 || rightBracket.length === 0) {
+  if (leftBracket.length === 0 || rightBracket.length === 0) {
     // 왼쪽 괄호, 오른쪽 괄호 없을때 예외처리
     result.push("NO");
-  } else if(temp.length === 0){
+  } else if (temp.length === 0) {
     result.push("YES");
   } else {
     result.push("NO");
