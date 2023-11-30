7d87a485e3d25a12943908e0539fb6fd68ea4f3e
tjrehdrms123 <wwe5455@naver.com>
2023-11-30 14:23:57 +0900

    feat: anwser

---
+++ study/CodingTest/Programmers/Code/2022 KAKAO TECH INTERNSHIP/성격 유형 검사하기/answer.js
@@ -0,0 +1,19 @@
+const survey = ["TR", "RT", "TR"];
+const choices = [7, 1, 3];
+
+function solution(survey, choices) {
+  const data = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+
+  for (let i = 0; i < survey.length; i++) {
+    const score = choices[i] - 4;
+    let type = survey[i].split("")[score < 0 ? 0 : 1];
+    data[type] += Math.abs(score);
+  }
+
+  const { R, T, C, F, J, M, A, N } = data;
+  return `${R >= T ? "R" : "T"}${C >= F ? "C" : "F"}${J >= M ? "J" : "M"}${
+    A >= N ? "A" : "N"
+  }`;
+}
+
+solution(survey, choices);
